fc2b52c8882293e56faea57b7a2bb366866313f3
Echelebo <echelebochibuike@gmail.com>
2024-12-17 13:16:00 +0100

    first

--- core/resources/views/emails/NewRegister.blade.php
+++ core/resources/views/emails/NewRegister.blade.php
@@ -4,7 +4,7 @@
     We wish to inform you that your Online Banking Registration Profile has been created successfully and your login details
     have been kept confidential and secured. Have a nice banking experience.
 
-    # <span style="color: {{ $textColor }}">BELOW ARE YOUR BANKING DETAILS</span>
+    BELOW ARE YOUR BANKING DETAILS!
 
     Email Address: {{ $demo->email }}
     Account Number: {{ $demo->acct }}
